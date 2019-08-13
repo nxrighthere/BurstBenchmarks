7331ed16f4abd529081630423ca47f15e8ae6d6d
NX <nxrighthere@gmail.com>
2019-08-13 18:44:24 +0500

    Fix stack overflow in Sieve of Eratosthenes

--- benchmarks.c
+++ benchmarks.c
@@ -231,16 +231,16 @@ EXPORT uint32_t benchmark_sieve_of_eratosthenes(uint32_t iterations) {
 	for (a = 1; a <= iterations; a++) {
 		count = 0;
 
-		for (b = 0; b <= size; b++) {
+		for (b = 0; b < size; b++) {
 			flags[b] = 1; // True
 		}
 
-		for (b = 0; b <= size; b++) {
+		for (b = 0; b < size; b++) {
 			if (flags[b] == 1) {
 				prime = b + b + 3;
 				c = b + prime;
 
-				while (c <= size) {
+				while (c < size) {
 					flags[c] = 0; // False
 					c += prime;
 				}
